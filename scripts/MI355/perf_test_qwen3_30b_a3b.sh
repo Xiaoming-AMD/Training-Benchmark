8b4b7fa83957c149f100f9230755538bd475ef9d
wenxie-amd <wen.xie@amd.com>
2026-01-04 06:00:06 +0000

    update qwen3 30b, fix tp issue

--- scripts/MI355/perf_test_qwen3_30b_a3b.sh
+++ scripts/MI355/perf_test_qwen3_30b_a3b.sh
@@ -7,31 +7,33 @@ TRAINING_BENCHMARK_DIR="$( dirname "$( dirname "$SCRIPT_DIR" )" )"
 PRIMUS_PATH="$TRAINING_BENCHMARK_DIR/third_party/Primus"
 
 export MODEL_NAME="qwen3_30B_A3B"
-export MANUAL_GC=True
+export MANUAL_GC=False
 export NUMA_BINDING=True
 export ENABLE_SYNC_FREE_MOE=True
 export ENABLE_TURBO_DEEPEP=True
 
-# NNODES, MBS, GBS, TP, PP, EP, RECOMPUTE_LAYERS, TRAIN_ITERS
 CONFIGS=(
-    "4 1 16 1 2 8 0 10"
-    "4 8 256 1 1 8 4 10"
-    "4 8 1024 1 1 8 4 10"
-    "8 1 16 4 1 8 0 10"
-    "8 4 256 1 1 8 0 10"
-    "8 8 1024 1 1 8 2 10"
+#   NNODES  MBS   GBS    TP  PP  EP  CP  RECOMPUTE_LAYERS  TRAIN_ITERS  LOG_AVG_SKIP_ITERATIONS
+    "4       1     16     1   2   8   1   0                 10          3"
+    "4       8    256     1   1   8   1   4                 10          3"
+    "4       8   1024     1   1   8   1   4                 10          3"
+    "8       1     16     4   1   8   1   0                 10          3"
+    "8       4    256     1   1   8   1   0                 10          3"
+    "8       8   1024     1   1   8   1   2                 10          3"
 )
 
 cd "$PRIMUS_PATH" || exit
 for CONFIG in "${CONFIGS[@]}"; do
-    read -r node_num mbs gbs tp pp ep recompute_layers train_iters <<< "$CONFIG"
+    read -r node_num mbs gbs tp pp ep cp recompute_layers train_iters log_avg_skip_iterations <<< "$CONFIG"
     export NNODES=${node_num}
     export MBS=${mbs}
     export GBS=${gbs}
     export TP=${tp}
     export PP=${pp}
     export EP=${ep}
+    export CP=${cp}
     export RECOMPUTE_LAYERS=${recompute_layers}
     export TRAIN_ITERS=${train_iters}
+    export LOG_AVG_SKIP_ITERATIONS=${log_avg_skip_iterations}
     bash "${TRAINING_BENCHMARK_DIR}"/scripts/MI355/run_pretrain_mi355x.sh
 done
